898623cf38600777deacd8e2a2e297889551fdeb
BAayah <138316674+BAayah@users.noreply.github.com>
2023-09-07 09:26:33 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let insert = document.getElementById('insert')
+
+document.addEventListener('keydown',(event)=> {
+    console.log(event.keyCode)
+    insert.innerHTML = `
+    <div class="key">
+      ${event.key}
+      <small>event.key</small>
+    </div>
+    <div class="key">
+      ${event.keyCode}
+      <small>event.keyCode</small>
+    </div>
+    <div class="key">
+      ${event.code}
+      <small>event.keyCode</small>
+    </div>`
+})
